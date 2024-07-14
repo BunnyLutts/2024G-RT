2441022c29c5a12c18da7c26e47a3512450cb898
BunnyLutts <lutts@qq.com>
2024-07-14 23:13:59 +0800

    Finish book2

--- RayTracer/src/main.rs
+++ RayTracer/src/main.rs
@@ -2,10 +2,10 @@ mod aabb;
 mod color;
 mod hittable;
 mod material;
+mod medium;
 mod quad;
 mod ray;
 mod sphere;
-mod medium;
 mod texture;
 mod utils;
 
@@ -20,6 +20,7 @@ use crate::ray::{Camera, CameraConfig};
 use crate::sphere::Sphere;
 use crate::texture::*;
 use crate::utils::{rand01, v3, Vec3};
+use std::collections::btree_set::Difference;
 use std::fs::File;
 use std::sync::Arc;
 
@@ -448,8 +449,16 @@ fn cornell_smoke() -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
         v3(130.0, 0.0, 65.0),
     ));
 
-    world.add(Arc::new(ConstantMedium::create(box1, 0.01, v3(0.0, 0.0, 0.0))));
-    world.add(Arc::new(ConstantMedium::create(box2, 0.01, v3(1.0, 1.0, 1.0))));
+    world.add(Arc::new(ConstantMedium::create(
+        box1,
+        0.01,
+        v3(0.0, 0.0, 0.0),
+    )));
+    world.add(Arc::new(ConstantMedium::create(
+        box2,
+        0.01,
+        v3(1.0, 1.0, 1.0),
+    )));
 
     let cam = Camera::new(CameraConfig {
         ratio: 1.0,
@@ -468,8 +477,133 @@ fn cornell_smoke() -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
     cam.render(&world)
 }
 
+fn final_scene(
+    img_width: usize,
+    sample_times: usize,
+    reflect_depth: usize,
+) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
+    let mut boxes1 = HittableList::new();
+    let ground = Arc::new(Lambertian::from(v3(0.48, 0.83, 0.53)));
+
+    let boxes_per_side = 20;
+    for i in 0..boxes_per_side {
+        for j in 0..boxes_per_side {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let z0 = -1000.0 + j as f64 * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = rand01() * 100.0 + 1.0;
+            let z1 = z0 + w;
+
+            boxes1.add(makebox(&v3(x0, y0, z0), &v3(x1, y1, z1), ground.clone()));
+        }
+    }
+
+    let mut world = HittableList::new();
+    world.add(Arc::new(BVHNode::from(boxes1)));
+
+    let light = Arc::new(DiffuseLight::from(v3(7.0, 7.0, 7.0)));
+    world.add(Arc::new(Quad::new(
+        v3(123.0, 554.0, 147.0),
+        v3(300.0, 0.0, 0.0),
+        v3(0.0, 0.0, 265.0),
+        light.clone(),
+    )));
+
+    let center1 = v3(400.0, 400.0, 200.0);
+    let center2 = center1 + v3(30.0, 0.0, 0.0);
+    let sphere_material = Arc::new(Lambertian::from(v3(0.7, 0.3, 0.1)));
+    world.add(Arc::new(Sphere::new(
+        center1,
+        center2 - center1,
+        50.0,
+        sphere_material.clone(),
+    )));
+
+    world.add(Arc::new(Sphere::stable_new(
+        v3(260.0, 150.0, 45.0),
+        50.0,
+        Arc::new(Dielectric::new(1.5)),
+    )));
+    world.add(Arc::new(Sphere::stable_new(
+        v3(0.0, 150.0, 145.0),
+        50.0,
+        Arc::new(Metal::new(v3(0.8, 0.8, 0.9), 1.0)),
+    )));
+
+    let boundary = Arc::new(Sphere::stable_new(
+        v3(360.0, 150.0, 145.0),
+        70.0,
+        Arc::new(Dielectric::new(1.5)),
+    ));
+    world.add(boundary.clone());
+    world.add(Arc::new(ConstantMedium::create(
+        boundary.clone(),
+        0.2,
+        v3(0.2, 0.4, 0.9),
+    )));
+    let boundary = Arc::new(Sphere::stable_new(
+        v3(0.0, 0.0, 0.0),
+        5000.0,
+        Arc::new(Dielectric::new(1.5)),
+    ));
+    world.add(Arc::new(ConstantMedium::create(
+        boundary.clone(),
+        0.0001,
+        v3(1.0, 1.0, 1.0),
+    )));
+
+    let emat = Arc::new(Lambertian::new(Arc::new(
+        ImageTexture::load("image/earthmap.jpeg").unwrap(),
+    )));
+    world.add(Arc::new(Sphere::stable_new(
+        v3(400.0, 200.0, 400.0),
+        100.0,
+        emat.clone(),
+    )));
+    let pertext = Arc::new(NoiseTexture::new(0.2));
+    world.add(Arc::new(Sphere::stable_new(
+        v3(220.0, 280.0, 300.0),
+        80.0,
+        Arc::new(Lambertian::new(pertext.clone())),
+    )));
+
+    let mut boxes2 = HittableList::new();
+    let white = Arc::new(Lambertian::from(v3(0.73, 0.73, 0.73)));
+    let ns = 1000;
+    for j in 0..ns {
+        boxes2.add(Arc::new(Sphere::stable_new(
+            Vec3::random_in(0.0, 165.0),
+            10.0,
+            white.clone(),
+        )));
+    }
+
+    world.add(Arc::new(Translation::new(
+        Arc::new(RotationY::new(Arc::new(BVHNode::from(boxes2)), 15.0)),
+        v3(-100.0, 270.0, 395.0),
+    )));
+
+    let cam = Camera::new(CameraConfig {
+        ratio: 1.0,
+        img_width,
+        sample_times,
+        reflect_depth,
+        background: v3(0.0, 0.0, 0.0),
+        vfov: 40.0,
+        lookfrom: Vec3::new(478.0, 278.0, -600.0),
+        lookat: Vec3::new(278.0, 278.0, 0.0),
+        vup: Vec3::new(0.0, 1.0, 0.0),
+        focus_dist: 10.0,
+        defocus_angle: 0.0,
+    });
+
+    cam.render(&BVHNode::from(world))
+}
+
 fn main() {
-    const SCENE: i32 = 8;
+    const SCENE: i32 = 9;
 
     let path = "output/test.jpg";
     let quality = 60;
@@ -484,7 +618,8 @@ fn main() {
         6 => simple_light(),
         7 => cornell_box(),
         8 => cornell_smoke(),
-        _ => bouncing_spheres(),
+        9 => final_scene(1080, 10000, 40),
+        _ => final_scene(400, 250, 4),
     });
     let mut output_file: File = File::create(path).unwrap();
     match output_image.write_to(&mut output_file, image::ImageOutputFormat::Jpeg(quality)) {
